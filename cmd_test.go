e8a9448e64e9fbce0ab83f2dc418e779e37a308f
robhurring <robhurring@gmail.com>
2015-10-13 19:36:01 -0400

    + test

---
+++ cmd_test.go
@@ -0,0 +1,45 @@
+package cmd
+
+import "testing"
+
+func TestName(t *testing.T) {
+	cmd := New("ls")
+
+	if cmd.Name != "ls" {
+		t.Errorf("Expected .Name to be 'ls' but instead it was '%s'", cmd.Name)
+	}
+}
+
+func TestString(t *testing.T) {
+	cmd := New("ls").WithArgs("-a", "-l")
+
+	if cmd.String() != "ls -a -l" {
+		t.Errorf("Expected .String() to be 'ls -a -l' but got '%s'", cmd.String())
+	}
+}
+
+func TestArgs(t *testing.T) {
+	cmd := New("ls").WithArgs("-a", "-l")
+
+	if len(cmd.Args) != 2 {
+		t.Errorf("Expected 2 args, got %d", len(cmd.Args))
+	}
+}
+
+func TestCombinedOutput(t *testing.T) {
+	cmd := New("ls")
+
+	_, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Errorf("Expected output, got error '%s'", err)
+	}
+}
+
+func TestRun(t *testing.T) {
+	cmd := New("ls")
+
+	err := cmd.Run()
+	if err != nil {
+		t.Errorf("Expected no errors, got error '%s'", err)
+	}
+}
